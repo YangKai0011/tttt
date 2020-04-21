c4a1abd537c75f20ccb9579bd03f58dba46659b3
yuki <443926471@qq.com>
2020-04-21 20:16:50 +0800

    update

--- routes/appraisal.js
+++ routes/appraisal.js
@@ -12,7 +12,7 @@ router.post('/addAppraiasl', async (req, res, next) => {
     const param = req.body;
     console.log(param);
     const add = new AllAdd();
-    const result = await add.addAppraisal([param.buildNumber, param.dormitoryNumber, param.violationssc, param.neatItemssc, param.score, param.optiones]);
+    const result = await add.addAppraisal([param.buildNumber, param.dormitoryNumber, param.violations, param.neatItems, param.score, param.options]);
     add.addResult();
     let status = statues(result);
     res.send({ status: status, result: result.results });
